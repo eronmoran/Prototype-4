ab3f631acc43915012b18a2ab699445ef04411bb
eronmoran <ermoran@s.sfusd.edu>
2023-03-06 14:44:19 -0800

    Added Powerup
    Still not functional

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
+    public bool hasPowerup;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +23,23 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hasPowerup = true;
+        Destroy(other.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+       if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+       {
+          Rigidbody EnemyRigidbody = collision.gameObject.GetComponent<EnemyRigidbody>();
+          Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+          enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+          Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+       }
     }
 }
