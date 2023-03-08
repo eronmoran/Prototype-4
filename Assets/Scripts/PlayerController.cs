8f90d346c20c0fb64796e43e71ec440ff9cd3e1b
eronmoran <ermoran@s.sfusd.edu>
2023-03-08 10:57:57 -0800

    Enemy Dies
    Enemy will destroy if it falls off

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     private GameObject focalPoint;
     private float powerUpStrength = 15.0f;
     public float speed = 5.0f;
-    public bool hasPowerup;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -23,19 +24,33 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        hasPowerup = true;
-        Destroy(other.gameObject);
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }    
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
        {
-          Rigidbody EnemyRigidbody = collision.gameObject.GetComponent<EnemyRigidbody>();
+          Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
           Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
 
           enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
